a66711a17aea4a72e487164973b50ec557d6dc4f
Lin Zhu <zhulin1362338@gmail.com>
2024-09-26 15:52:15 -0700

    Implemented restore redux store at page load using jwtDecode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
+import { jwtDecode } from 'jwt-decode';
 import './index.css';
 
 import store from './services/store.js';
+import { getCurrentUser } from './features/userSlice.js';
+
+const token = localStorage.getItem('token');
+if (token) {
+  store.dispatch(getCurrentUser(jwtDecode(token)));
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
