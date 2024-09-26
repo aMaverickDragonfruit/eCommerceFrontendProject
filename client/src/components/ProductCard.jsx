42101d009bb168794c9cbb7c3bd0a9a380c3a5fc
Lin Zhu <zhulin1362338@gmail.com>
2024-09-26 16:01:28 -0700

    Update Products and ProductCar component to use real data

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -4,25 +4,17 @@ import { useState } from 'react';
 const { Meta } = Card;
 
 export default function ProductCard({ product }) {
-  const { imgLink, productName, price } = product;
+  const { _id, userId, name, description, category, price, stock, imgUrl } =
+    product;
   //   const [counts, setCounts] = useState(0);
   //   console.log(imgLink, productName, price);
   return (
     <Card
       className='w-80 border-2'
       hoverable
-      cover={
-        <img
-          className=''
-          alt='example'
-          src={imgLink}
-        />
-      }
+      cover={<img className='' alt='example' src={imgUrl} />}
     >
-      <Meta
-        title={productName}
-        description={price}
-      />
+      <Meta title={name} description={price} />
       <Flex>
         <Button type='primary'>Add</Button>
         <Button type='text'>Edit</Button>
