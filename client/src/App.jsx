0bcd1fa58bb26fc8eea8160ff0be0d17bb1e15fa
XiaoweiweiZheng <xiaoweiweizheng@gmail.com>
2024-09-26 01:48:08 -0700

    adding application layout and set up Route Outlet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,26 +5,42 @@ import PwUpdate from './pages/PwUpdate';
 import PwUpdateNoti from './pages/PwUpdateNoti';
 import ProtectedRoute from './components/ProtectedRoute';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './components/Layout';
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Navbar /> */}
-        {/* <Footer /> */}
         <Routes>
-          <Route path='/signin' element={<SignIn />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/forgot-password' element={<PwUpdate />} />
-          <Route path='/email-sent' element={<PwUpdateNoti />} />
           <Route
-            path='/productes'
-            element={
-              <ProtectedRoute>
-                <Products />
-              </ProtectedRoute>
-            }
-          />
+            path='/'
+            element={<MainLayout />}
+          >
+            <Route
+              path='signin'
+              element={<SignIn />}
+            />
+            <Route
+              path='signup'
+              element={<SignUp />}
+            />
+            <Route
+              path='forgot-password'
+              element={<PwUpdate />}
+            />
+            <Route
+              path='email-sent'
+              element={<PwUpdateNoti />}
+            />
+            <Route
+              path='products'
+              element={
+                <ProtectedRoute>
+                  <Products />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
