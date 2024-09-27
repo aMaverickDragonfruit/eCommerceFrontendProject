35d65dc26f6f2b5eaca2f0e7f94443816d6bfc34
XiaoweiweiZheng <xiaoweiweizheng@gmail.com>
2024-09-27 02:39:10 -0700

    add testing route and file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,17 +8,33 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/Layout';
 import AddProduct from './pages/AddProduct';
 import ProductDetails from './pages/ProductDetail';
+import Testing from './components/testing';
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<MainLayout />}>
-            <Route path='signin' element={<SignIn />} />
-            <Route path='signup' element={<SignUp />} />
-            <Route path='forgot-password' element={<PwUpdate />} />
-            <Route path='email-sent' element={<PwUpdateNoti />} />
+          <Route
+            path='/'
+            element={<MainLayout />}
+          >
+            <Route
+              path='signin'
+              element={<SignIn />}
+            />
+            <Route
+              path='signup'
+              element={<SignUp />}
+            />
+            <Route
+              path='forgot-password'
+              element={<PwUpdate />}
+            />
+            <Route
+              path='email-sent'
+              element={<PwUpdateNoti />}
+            />
             <Route
               path='products'
               element={
@@ -35,8 +51,17 @@ export default function App() {
                 </ProtectedRoute>
               }
             />
-            <Route path='add-product' element={<AddProduct />} />
+            <Route
+              path='add-product'
+              element={<AddProduct />}
+            />
             {/* <Route path='product-detail' element={<DetailCard />} /> */}
+
+            {/* This is testing Route */}
+            <Route
+              path='testing'
+              element={<Testing />}
+            />
           </Route>
         </Routes>
       </BrowserRouter>
