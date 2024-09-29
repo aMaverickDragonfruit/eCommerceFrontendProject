b7c9a400921e65d735e7d19e8409d3363e5e302c
Lin Zhu <zhulin1362338@gmail.com>
2024-09-28 17:38:26 -0700

    Moved fetchPorducts to the outer layer of the App

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,11 +7,17 @@ import './index.css';
 
 import store from './services/store.js';
 import { getCurrentUser } from './features/userSlice.js';
+import { fetchCart } from './features/cartSlice.js';
+import { fetchProducts } from './features/productSlice';
 
 const token = localStorage.getItem('token');
 if (token) {
-  store.dispatch(getCurrentUser(jwtDecode(token)));
+  const jwtPayload = jwtDecode(token);
+  store.dispatch(getCurrentUser(jwtPayload));
+  const cartId = jwtPayload.user.cart;
+  store.dispatch(fetchCart(cartId));
 }
+store.dispatch(fetchProducts());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
