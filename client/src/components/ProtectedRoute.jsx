8239ad216f528928cfdc9225fa416c9cb21e85eb
Lin Zhu <zhulin1362338@gmail.com>
2024-10-01 13:35:16 -0700

    Updated ProtectedRoute to check if token is expired.

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,13 +1,29 @@
 import { Navigate, useLocation } from 'react-router-dom';
 import { useMemo } from 'react';
+import { jwtDecode } from 'jwt-decode';
 
 export default function ProtectedLayout({ children }) {
-  const user = useMemo(() => localStorage.getItem('token'), []);
+  const token = useMemo(() => localStorage.getItem('token'), []);
   const location = useLocation();
 
-  if (!user) {
+  if (!token) {
     return <Navigate to='/signin' state={{ from: location }} />;
   }
 
-  return <div>{children}</div>;
+  try {
+    const decoded = jwtDecode(token);
+    const { exp } = decoded;
+
+    const currentTime = Date.now() / 1000;
+
+    if (exp < currentTime) {
+      localStorage.removeItem('token');
+      return <Navigate to='/signin' state={{ from: location }} replace />;
+    } else {
+      return <>{children}</>;
+    }
+  } catch (error) {
+    console.error('Error decoding token:', error);
+    return <Navigate to='/signin' state={{ from: location }} replace />;
+  }
 }
